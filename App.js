33256e68638c97499bef11222e07a88663bf002e
demonik755 <ddima92@mail.ru>
2020-07-16 19:02:32 +0600

    New: SwipeLeft delete function Add Flatlist component

--- App.js
+++ App.js
@@ -1,38 +1,52 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, } from 'react-native';
+import {StyleSheet, View, FlatList } from 'react-native';
 import {NavBar} from "./src/components/Navbar";
 import {AddNews} from "./src/components/AddNews";
 import {News} from "./src/components/News";
 
 export default function App() {
-    const [news, setNews] = useState([]);
+    const [news, setNews] = useState([
+        {id: 1, title: "hello", description: "hello world1"},
+        {id: 2, title: "hello", description: "hello world2"},
+        {id: 3, title: "hello", description: "hello world3"},
+        {id: 4, title: "hello", description: "hello world4"},
+        {id: 5, title: "hello", description: "hello world5"},
+        {id: 6, title: "hello", description: "hello world6"},
+        {id: 7, title: "hello", description: "hello world7"},
+        {id: 8, title: "hello", description: "hello world8"},
+        {id: 9, title: "hello", description: "hello world9"},
+        {id: 10, title: "hello", description: "hello world10"},
+    ]);
     const addNews =(title,description) => {
         setNews(prev =>[
-            ...prev,
             {
                 id: Date.now().toString(),
                 title: title,
                 description:description,
-            }
+            },
+            ...prev,
         ])
+    };
+    const removeNews = id => {
+        setNews(prev => prev.filter(news=>news.id !==id))
     };
   return (
     <View>
         <NavBar/>
       <View style={styles.container}>
           <AddNews onSubmit={addNews}/>
-
+          <FlatList
+              keyExtractor={item => item.id.toString()}
+              data={news}
+              renderItem={({item}) => (<News news={item} onRemove={removeNews}/>) }/>
       </View>
-        <View>
-            {news.map(news=> <News key={news.id} news={news}/>)}
-        </View>
     </View>
-
   );
 }
 const styles = StyleSheet.create({
   container: {
-    paddingVertical:  40
+      paddingVertical:  40,
+      height: "90%",
   },
 });
 
--- src/components/News.js
+++ src/components/News.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import { StyleSheet,  View } from 'react-native';
+import { StyleSheet, View, Animated, TouchableOpacity } from 'react-native';
 import {Body, Container, Content, Left, List, ListItem, Right, Text, Thumbnail, Button} from "native-base";
-export const News = ({news}) => {
+import Swipeable from "react-native-gesture-handler/Swipeable";
+export const News = ({news, onRemove}) => {
+    const rightActions= (progress, dragX) => {
+        const scale = dragX.interpolate({
+            inputRange: [-50, 200],
+            outputRange: [1, 0],
+            extrapolate: 'clamp'
+        });
+        return(
+            <TouchableOpacity onLongPress={()=>onRemove(news.id)}>
+                <View style={styles.rightAction}>
+                    <Animated.Text style={[styles.actionText, {transform: [{scale}]}]}>
+                        Delete
+                    </Animated.Text>
+                </View>
+            </TouchableOpacity>
+
+        )
+    };
     return (
-        <View style={styles.news}>
+        <View style={styles.news} >
             <Container >
                 <Content>
                     <List>
-                        <ListItem thumbnail>
-                            <Left>
-                                <Thumbnail square source={{ uri: 'http://www.sensawine.com/Blog/wp-content/uploads/2012/03/news.jpg' }} />
-                            </Left>
-                            <Body>
-                                <Text >{news.title}</Text>
-                                <Text note numberOfLines={2}>{news.description}. .</Text>
-                            </Body>
-                            <Right>
-                                <Button transparent>
-                                    <Text>View</Text>
-                                </Button>
-                            </Right>
-                        </ListItem>
+                        <Swipeable renderRightActions={rightActions} >
+                            <ListItem thumbnail >
+                                <Left>
+                                    <Thumbnail square source={{ uri: 'http://www.sensawine.com/Blog/wp-content/uploads/2012/03/news.jpg' }} />
+                                </Left>
+                                <Body>
+                                    <Text >{news.title}</Text>
+                                    <Text note numberOfLines={2}>{news.description}. .</Text>
+                                </Body>
+                                <Right>
+                                    <Button transparent>
+                                        <Text>View</Text>
+                                    </Button>
+                                </Right>
+                            </ListItem>
+                        </Swipeable>
                     </List>
                 </Content>
             </Container>
@@ -30,6 +50,18 @@ export const News = ({news}) => {
 const styles = StyleSheet.create({
     news: {
         flexDirection: 'row',
-        height: 100
+        height: 100,
+    },
+    rightAction: {
+        backgroundColor: "red",
+        justifyContent: "center",
+        alignItems: "flex-end",
+
+    },
+    actionText:{
+        color: "#fff",
+        fontWeight: "600",
+        padding: 28,
     }
+
 });
